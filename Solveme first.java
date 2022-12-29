c0017f65039f68f0737adc96fca92b3cdeea7ce0
Mr.Brionez <edlerbriones@gmail.com>
2022-12-28 22:53:19 -0600

    Primer ejercicio

---
+++ Solveme first.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int a = stdin.nextInt();
+        int b = stdin.nextInt();
+        int suma = a + b;
+
+        System.out.println(suma);
+
+    }
+
+}
