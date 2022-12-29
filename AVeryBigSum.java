a74548749205d0f59b988646ad9c6fe0b320b178
Mr.Brionez <edlerbriones@gmail.com>
2022-12-28 23:14:48 -0600

    cuarto ejecricio

---
+++ AVeryBigSum.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class AVeryBigSum {
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<Long> ar) {
+        long results = 0;
+
+        for (long elemento : ar) {
+            results += elemento;
+
+        }
+        return results;
+    }
+
+}
